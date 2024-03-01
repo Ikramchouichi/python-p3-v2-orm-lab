947410e7b847ceb601e3ed291222c28f7e69043b
Ikram Chouichi <ikram@netronian.com>
2024-03-01 18:05:22 -0500

    ORM Lab

--- lib/employee.py
+++ lib/employee.py
@@ -4,7 +4,6 @@ from department import Department
 
 class Employee:
 
-    # Dictionary of objects saved to the database.
     all = {}
 
     def __init__(self, name, job_title, department_id, id=None):
@@ -135,15 +134,14 @@ class Employee:
     def instance_from_db(cls, row):
         """Return an Employee object having the attribute values from the table row."""
 
-        # Check the dictionary for  existing instance using the row's primary key
+        
         employee = cls.all.get(row[0])
         if employee:
-            # ensure attributes match row values in case local instance was modified
+
             employee.name = row[1]
             employee.job_title = row[2]
             employee.department_id = row[3]
         else:
-            # not in dictionary, create new instance and add to dictionary
             employee = cls(row[1], row[2], row[3])
             employee.id = row[0]
             cls.all[employee.id] = employee
